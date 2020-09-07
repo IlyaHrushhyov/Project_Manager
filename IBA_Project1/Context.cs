58f8e229ea3547a3ffbc6bc8d7e69302283bc87e
hrushhyov@gmail.com <hrushhyov@gmail.com>
2020-09-07 22:42:26 +0300

    Add project files.

---
+++ IBA_Project1/Context.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace IBA_Project1
+{
+    class Context : DbContext
+    {
+        public Context()
+            :base("DbConnection")
+        { }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
