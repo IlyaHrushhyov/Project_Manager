25b5dc1b1dadb85f08063e896abd8498fdf9d70b
hrushhyov@gmail.com <hrushhyov@gmail.com>
2020-09-17 00:04:15 +0300

    ViewModel class added

---
+++ IBA_Project1/ViewModel/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IBA_Project1.ViewModel
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+        
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
